958002fb3a11e8c8decbde917d0f8955aab49f48
madiwa Simon <mad.simon01@gmail.com>
2020-11-17 17:14:01 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 require 'pry'
 def key_for_min_value(name_hash)
   name_value = 99999999
-  min_value = ["simon", 99999999]
+  min_value = [ nil, 99999999]
   name_hash.each do |name, value|
     if value < name_value && value < min_value[1]
       min_value = [name, value]
